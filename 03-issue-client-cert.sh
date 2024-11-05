04a5fc11e2992e4bcc2cffbe5f5f25fda1391f4e
zmstone <zmstone@gmail.com>
2024-11-05 08:58:43 +0100

    Parameterise client key and cert file name

--- 03-issue-client-cert.sh
+++ 03-issue-client-cert.sh
@@ -23,16 +23,18 @@ CL_O="${TLS_DN_O:-MyOrgName}"
 CL_OU="${TLS_DN_OU:-MyServiceClient}"
 CL_CN="${TLS_CLIENT_COMMON_NAME:-localhost}"
 
-if [ ! -f client.key ]; then
+FNAME="${TLS_CLIENT_FNAME:-client}"
+
+if [ ! -f "${FNAME}.key" ]; then
   case $ALG in
     rsa)
-      openssl genrsa -out client.key 2048
+      openssl genrsa -out "${FNAME}.key" 2048
       ;;
     ec|ecc)
-      openssl ecparam -name prime256v1 -genkey -noout -out client.key
+      openssl ecparam -name prime256v1 -genkey -noout -out "${FNAME}.key"
       ;;
     dsa)
-      openssl gendsa -out "client.key" <(openssl dsaparam 2048)
+      openssl gendsa -out "${FNAME}.key" <(openssl dsaparam 2048)
       ;;
     *)
       echo "Unknown algorithm: $ALG"
@@ -41,8 +43,8 @@ if [ ! -f client.key ]; then
   esac
 fi
 
-if [ ! -f client.pem ]; then
-  openssl req -sha256 -new -key client.key -out client.csr -nodes -subj "/C=${CL_C}/ST=${CL_ST}/L=${CL_L}/O=${CL_O}/OU=${CL_OU}/CN=${CL_CN}" -addext "basicConstraints=critical,CA:false"
-  openssl x509 -req -CA "${ISSUER_CA}.pem" -CAkey "${ISSUER_CA}.key" -in client.csr -out client.pem -days 3650  -CAcreateserial
+if [ ! -f "${FNAME}" ]; then
+  openssl req -sha256 -new -key "${FNAME}.key" -out client.csr -nodes -subj "/C=${CL_C}/ST=${CL_ST}/L=${CL_L}/O=${CL_O}/OU=${CL_OU}/CN=${CL_CN}" -addext "basicConstraints=critical,CA:false"
+  openssl x509 -req -CA "${ISSUER_CA}.pem" -CAkey "${ISSUER_CA}.key" -in client.csr -out "${FNAME}.pem" -days 3650  -CAcreateserial
   rm -f client.csr
 fi
