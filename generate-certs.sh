73e25c6eedede373c7259e2fab86bcac0be3f430
zmstone <zmstone@gmail.com>
2025-03-31 23:51:27 +0200

    Allow user set RSA_PSS_SIGNOPTS

--- generate-certs.sh
+++ generate-certs.sh
@@ -11,7 +11,8 @@ clean() {
 
 clean
 
-export RSA_PSS_SIGNOPTS='-sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:20'
+DEFAULT_RSA_PSS_SIGNOPTS='-sigopt rsa_padding_mode:pss -sigopt rsa_pss_saltlen:20'
+export RSA_PSS_SIGNOPTS="${RSA_PSS_SIGNOPTS-${DEFAULT_RSA_PSS_SIGNOPTS}}"
 
 export ALG="${ALG:-rsa}"
 ./00-generate-root-ca.sh
